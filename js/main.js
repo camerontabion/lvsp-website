a21d8f87561cce5e6d4c4582c51ac912317c1072
Cameron Keokolo Tabion <cameronkeokolotabion@gmail.com>
2021-01-24 21:18:47 -0800

    Fixed button not found error

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const header = document.querySelector('header');
 const menuBtn = header.querySelector('.menu-btn');
 
-menuBtn.addEventListener('click', () => {
-  if (header.classList.contains('menu-open')) {
-    header.classList.remove('menu-open');
-  } else {
-    header.classList.add('menu-open');
-  }
-});
+if (menuBtn) {
+  menuBtn.addEventListener('click', () => {
+    if (header.classList.contains('menu-open')) {
+      header.classList.remove('menu-open');
+    } else {
+      header.classList.add('menu-open');
+    }
+  });
+}
